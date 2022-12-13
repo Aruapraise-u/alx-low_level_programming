6904a346fd16eac3c8d23e0366c9cdc713544f30
Aruapraise-u <aruaobinna46@gmail.com>
2022-12-13 09:47:19 +0000

    mm

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ *main - entry point
+ *
+ * Return: nothing. return viod
+ */
+
+int main(viod)
+	(
+	 _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+	 _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 -putchar('\n');
+
+	 return(0);
+	)
+
